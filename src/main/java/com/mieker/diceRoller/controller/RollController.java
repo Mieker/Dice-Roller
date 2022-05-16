28183555234f1f1c6e2b1ba76e6ff650abf2674d
Mieker <mieker@wp.pl>
2022-05-16 14:38:55 +0200

    mvn plugin restore

--- src/main/java/com/mieker/diceRoller/controller/RollController.java
+++ src/main/java/com/mieker/diceRoller/controller/RollController.java
@@ -12,7 +12,7 @@ import java.io.FileNotFoundException;
 
 
 @RestController
-@CrossOrigin(origins = "https://dicethrowerapp-front.herokuapp.com/roll/d6/")
+@CrossOrigin(origins = "https://dicethrowerapp-front.herokuapp.com/roll/d6/", maxAge = 3600)
 @RequestMapping("/roll")
 public class RollController {
 
